cd32dc8b6ef4725282b2526a1b435d037c81b011
vandosant <sjskender@gmail.com>
2018-02-26 08:25:47 -0700

    Reset players and restrict pickWinner calls

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -9,7 +9,7 @@ contract Lottery {
     }
 
     function enter() public payable {
-        require(msg.value == .01 ether);
+        require(msg.value == 0.01 ether);
 
         players.push(msg.sender);
     }
@@ -18,8 +18,18 @@ contract Lottery {
         return uint(keccak256(block.difficulty, now, players));
     }
 
-    function pickWinner() public {
+    function pickWinner() public restricted {
         uint index = random() % players.length;
         players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
     }
 }
