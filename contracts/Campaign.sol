945d7bdedd6c961082db83afd4708b6a0f89e9cb
vandosant <sjskender@gmail.com>
2018-03-14 19:35:09 -0600

    Adds CampaignFactory contract

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,3 +1,16 @@
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
 contract Campaign {
     struct Request {
         string description;
@@ -19,8 +32,8 @@ contract Campaign {
         _;
     }
 
-    function Campaign(uint minimum) public {
-        manager = msg.sender;
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
         minimumContribution = minimum;
     }
 
