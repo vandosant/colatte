1167a994d5b278e924d64698bfe5e353d6a6b91e
vandosant <sjskender@gmail.com>
2018-03-16 12:27:20 -0600

    Display campaign cards

--- crowdsource/pages/index.js
+++ crowdsource/pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Card } from 'semantic-ui-react'
 import factory from '../ethereum/factory'
 
 export default class Index extends Component {
@@ -11,6 +12,15 @@ export default class Index extends Component {
   }
 
   render () {
-    return <div>{this.props.campaigns[0]}</div>
+    const items = this.props.campaigns.map(c => ({
+      header: c,
+      description: <a>View Campaign</a>,
+      fluid: true
+    }))
+    return (<div>
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+      <Card.Group items={items} />
+      </div>
+    )
   }
 }
