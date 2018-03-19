77d5a75ebcdeca1e2fc8ab4ed62ab9f3c802501d
vandosant <sjskender@gmail.com>
2018-03-19 16:49:57 -0600

    Add ability to approve & finalize requests

--- crowdsource/pages/campaigns/requests/index.js
+++ crowdsource/pages/campaigns/requests/index.js
@@ -4,7 +4,7 @@ import {
   Table
 } from 'semantic-ui-react'
 import Layout from '../../../components/Layout'
-import { Link } from '../../../routes'
+import { Link, Router } from '../../../routes'
 import getCampaign from '../../../ethereum/campaign'
 import web3 from '../../../ethereum/web3'
 
@@ -20,17 +20,50 @@ export default class RequestIndex extends Component {
         return campaign.methods.requests(index).call()
       })
     )
+    const summary = await campaign.methods.getSummary().call()
 
     return {
       address: props.query.address,
+      approversCount: summary[3],
       requests,
       requestCount
+    }
+  }
+
+  async handleApprove (id) {
+    try {
+      const campaign = getCampaign(this.props.address)
+      const accounts = await web3.eth.getAccounts()
+      await campaign.methods.approveRequest(id).send({
+        from: accounts[0]
+      })
+      Router.replaceRoute(`/campaigns/${this.props.address}/requests`)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async handleFinalize (id) {
+    try {
+      const campaign = getCampaign(this.props.address)
+      const accounts = await web3.eth.getAccounts()
+      await campaign.methods.finalizeRequest(id).send({
+        from: accounts[0]
+      })
+      Router.replaceRoute(`/campaigns/${this.props.address}/requests`)
+    } catch (err) {
+      console.error(err)
     }
   }
 
   render () {
     return (
       <Layout>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary floated='right' style={{ marginBottom: '1em' }}>Add Request</Button>
+          </a>
+        </Link>
         <h3>Request List</h3>
         <Table>
           <Table.Header>
@@ -45,21 +78,43 @@ export default class RequestIndex extends Component {
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {this.props.requests.map((request, index) => <Table.Row key={index}>
-                <Table.Cell>{index}</Table.Cell>
-                <Table.Cell>{request.description}</Table.Cell>
-                <Table.Cell>{web3.utils.fromWei(request.value, 'ether')}</Table.Cell>
-                <Table.Cell>{request.recipient}</Table.Cell>
-                <Table.Cell>{request.approvalCount}</Table.Cell>
-              </Table.Row>
-            )}
+            {this.props.requests.map((request, index) => {
+              const isApproved = request.approvalCount > this.props.approversCount / 2
+              return (
+                <Table.Row
+                  key={index}
+                  disabled={request.completed}
+                  positive={isApproved && !request.completed}
+                >
+                  <Table.Cell>{index}</Table.Cell>
+                  <Table.Cell>{request.description}</Table.Cell>
+                  <Table.Cell>{web3.utils.fromWei(request.value, 'ether')}</Table.Cell>
+                  <Table.Cell>{request.recipient}</Table.Cell>
+                  <Table.Cell>{request.approvalCount} / {this.props.approversCount}</Table.Cell>
+                  <Table.Cell>
+                    {request.completed ? null : (
+                      <Button
+                        onClick={() => this.handleApprove(index)}
+                        basic
+                        color='green'
+                      >Approve</Button>
+                    )}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {request.completed ? null : (
+                      <Button
+                        onClick={() => this.handleFinalize(index)}
+                        basic
+                        color='teal'
+                      >Finalize</Button>
+                    )}
+                  </Table.Cell>
+                </Table.Row>
+              )
+            })}
           </Table.Body>
         </Table>
-        <Link route={`/campaigns/${this.props.address}/requests/new`}>
-          <a>
-            <Button primary>Add Request</Button>
-          </a>
-        </Link>
+        <div>Found {this.props.requestCount} requests.</div>
       </Layout>
     )
   }
